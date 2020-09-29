14d7855d1ab2fc59ef4e2f9a26189cff41f6b23a
villoldo <villoldo19805@iesmarenostrum.com>
2020-09-29 19:46:34 +0200

    Equationprueba
    prueba de subida del ejercicio de equation solve

---
+++ Prueba defi/Prueba defi/EquationSolve1.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Prueba_defi
+{
+    class EquationSolve1
+    {
+
+        public static double SolveEquation1(double a, double b, double c)
+        {
+            double aux, aux2, x;
+
+            aux = b * b - 4.0 * a * c;
+            aux2 = System.Math.Sqrt(aux);
+            x = (-b + aux2) / (2.0 * a);
+            return x;
+        }
+        public static double SolveEquation2(double a, double b)
+        {
+            double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
---
+++ Prueba defi/Prueba defi/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1, s2, s3, s4, s5;
+            {
+                s1 = EquationSolve1.SolveEquation1(2.0, 3.0, 5.0);
+                System.Console.WriteLine("El resultado de la ecuacion es " + s1);
+
+                s2 = EquationSolve1.SolveEquation1(3.0, 4.0, 1.0);
+
+                System.Console.WriteLine("El resultado de la ecuacion es " + s2);
+
+                s3 = EquationSolve1.SolveEquation1(2.0, 3.0, 1.0);
+
+                System.Console.WriteLine("El resultado de la ecuacion es " + s3);
+
+                System.Console.WriteLine("El resultado de la suma de las dos ecuaciones es " + (s3 + s2));
+
+                s4 = EquationSolve1.SolveEquation2(1.0, -3.0);
+
+                System.Console.WriteLine("El resultado de la ecuación de primer grado es" + s4);
+
+                s5 = EquationSolve1.SolveEquation2(2.0, -5.0);
+
+                System.Console.WriteLine("El resultado de la ecuación de primer grado es" + s5);
+
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
