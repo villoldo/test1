2d7ceace7ccb8fc2dc57598417487576870fdb62
villoldo <villoldo19805@iesmarenostrum.com>
2020-09-29 20:00:09 +0200

    geometry class
    clase de geometria del dia 29 de sep

---
+++ Geometria/Geometria/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Geometria
+{ class Geometry
+{
+    public static double GetVectorLenght(double x, double y)
+    {
+        double aux;
+        aux = x * x + y * y;
+        double lenght;
+        lenght = System.Math.Sqrt(aux);
+        return lenght;
+    }
+    static void Main(string[] args)
+    {
+        double s1;
+
+        s1 = Geometry.GetVectorLenght(1.0, 1.0);
+        System.Console.WriteLine("El resultado de la ecuacion es " + s1);
+    }
+
+
+}
+}
